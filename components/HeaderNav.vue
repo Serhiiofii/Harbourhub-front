37ebc693d7575a1af296dcc32dccea9dfc56bf16
Andrii <softwaredev210@gmail.com>
2022-09-27 07:00:30 -1100

    headernav commit

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,12 +1,18 @@
-<!-- Please remove this file from your project -->
 <template>
-  <div class="flex">
-    hello world
+  <div class="flex p-4 mx-10 justify-between">
+    <div>
+      <img class="my-auto" src="/logo.svg" alt="" />
+    </div>
+    <div>
+      <button class="p-3 bg-blue-600 rounded-md text-white w-52">
+        Create an Account
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "NuxtTutorial",
+  name: "HeaderNav",
 };
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'harbour-hub',
+    title: 'Harbour Hub',
     htmlAttrs: {
       lang: 'en'
     },
@@ -18,7 +18,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 
