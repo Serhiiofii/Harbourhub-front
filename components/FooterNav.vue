3ad62f0bc7f70beb2cf2221c7003957c318e22b2
Andrii <softwaredev210@gmail.com>
2022-11-17 01:22:22 -1100

    commit

--- components/FooterNav.vue
+++ components/FooterNav.vue
@@ -10,29 +10,35 @@
       </div>
       <div class="w-40">
         <div class="font-bold mb-4">Company</div>
-        <div class="my-1">Home</div>
-        <div class="my-1">Become a vendor</div>
-        <div class="my-1">About Us</div>
-        <div class="my-1">Privacy Policy</div>
+        <NuxtLink to="/"> <div class="my-1">Home</div> </NuxtLink>
+        <NuxtLink to="/becomeaseller"> <div class="my-1">Become a vendor</div> </NuxtLink>
+        <NuxtLink to="/"> <div class="my-1">About Us</div> </NuxtLink>
+        <NuxtLink to="/"> <div class="my-1">Privacy Policy</div> </NuxtLink>
       </div>
       <div class="w-40">
         <div class="font-bold mb-4">Quick Links</div>
-        <div class="my-1">Login</div>
-        <div class="my-1">Sign Up</div>
-        <div class="my-1">Cart</div>
-        <div class="my-1">Vendors</div>
+        <NuxtLink to="/login"> <div class="my-1">Login</div> </NuxtLink>
+        <NuxtLink to="/signup"> <div class="my-1">Sign Up</div> </NuxtLink>
+        <NuxtLink to="/my-orders"> <div class="my-1">Cart</div> </NuxtLink>
+        <NuxtLink to="/"> <div class="my-1">Vendors</div> </NuxtLink>
       </div>
       <div>
         <div class="font-bold mb-4">Join Our Newsletter</div>
         <div class="flex">
-          <input type="text" class="rounded-l-full p-3 border border-black w-52" placeholder="Your email address" />
+          <input
+            type="text"
+            class="rounded-l-full p-3 border border-black w-52"
+            placeholder="Your email address"
+          />
           <input
             type="button"
             class="rounded-r-full p-3 bg-blue-600 text-white w-32"
             value="Subscribe"
           />
         </div>
-        <div class="text-xs mt-1">* Will send you weekly updates for your healthy meals</div>
+        <div class="text-xs mt-1">
+          * Will send you weekly updates for your healthy meals
+        </div>
       </div>
     </div>
     <div class="p-4 text-center font-bold text-sm">
